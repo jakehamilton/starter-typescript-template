07a7574785596ee36f541cc9ab1a01d28ed66f92
Jake Hamilton <jehamilton122@gmail.com>
2018-08-02 14:55:15 -0700

    fix(package): Build and test before deploy.
    This also includes a small tweak for Wallaby so that the tool can
    recognize jest mocks.

--- wallaby.js
+++ wallaby.js
@@ -4,13 +4,14 @@
 /**
  * The wallaby config factory
  * @param {wallabyjs.IWallaby} wallaby The wallaby instance
- * 
+ *
  * @returns {wallabyjs.IWallabyConfig} The wallaby config to be used
  */
 module.exports = wallaby => ({
     files: [
         'src/**/*.ts?(x)',
         'src/**/*.snap',
+        'src/**/__mocks__/**',
         '!src/**/*.spec.ts?(x)',
         '!src/**/__tests__/**/*.ts?(x)',
     ],
